84709adca94a7cd8247884050058f3dfcac47a68
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2021-03-10 08:30:28 +0100

    cart screen added

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import Message from "../components/Message";
+import { addToCart } from "../actions/cartActions";
 
-const CartScreen = () => {
+const CartScreen = ({ match, location, history }) => {
+  // get product id from url
+  const productId = match.params.id;
+  // get quantity from url params
+  const qty = location.search ? Number(location.search.split("=")[1]) : 1;
+  // define dispatch
+  const dispatch = useDispatch();
+  // get cart items read from local storage to redux as an initial state
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
+
   return <div style={{ marginTop: "20rem" }}>cart</div>;
 };
 
