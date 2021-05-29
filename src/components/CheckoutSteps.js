259c3510ac484cda1677611f51985b3c7c3d601b
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2021-05-29 22:20:53 +0200

    profile screen styled

--- src/components/CheckoutSteps.js
+++ src/components/CheckoutSteps.js
@@ -10,7 +10,6 @@ const useStyles = makeStyles((theme) => ({
     padding: "3rem 3rem 2rem",
     ...theme.typography.mont,
     opacity: 0.8,
-    // border: "1px solid green",
   },
   containerPassive: {
     ...theme.flex.row,
@@ -20,7 +19,6 @@ const useStyles = makeStyles((theme) => ({
     margin: "auto",
     marginBottom: "2rem",
     ...theme.typography.mont,
-    // border: "1px solid magenta",
     position: "relative",
     "&::before": {
       content: "''",
@@ -83,7 +81,6 @@ const useStyles = makeStyles((theme) => ({
     margin: "auto",
     marginBottom: "2rem",
     ...theme.typography.mont,
-    // border: "1px solid magenta",
     position: "relative",
     "&::before": {
       content: "''",
@@ -139,153 +136,16 @@ const useStyles = makeStyles((theme) => ({
     left: "50%",
     transform: "translateX(-50%)",
     color: "inherit",
-    // border: "1px solid orange",
   },
-  // container: {
-  //   ...theme.utils.container,
-  //   ...theme.typography.mont,
-  //   paddingTop: "3rem",
-  // },
-  // nav: {
-  //   ...theme.flex.row,
-  //   listStyle: "none",
-  //   "& > *": {
-  //     padding: ".5rem 2rem",
-  //   },
-  // },
-  // liActive: {
-  //   ...theme.flex.col,
-  //   color: theme.palette.primary.light,
-  // },
-  // liDisabled: {
-  //   ...theme.flex.row,
-  //   color: theme.palette.text.disabled,
-  // },
-  // circleActive: {
-  //   ...theme.flex.col,
-  //   padding: ".5rem",
-  //   backgroundColor: "white",
-  //   color: theme.palette.primary.light,
-  //   border: `2px solid ${theme.palette.primary.light}`,
-  //   width: 30,
-  //   height: 30,
-  //   borderRadius: "50%",
-  // },
-  // circleDisabled: {
-  //   ...theme.flex.col,
-  //   padding: ".5rem",
-  //   backgroundColor: "white",
-  //   border: `2px solid ${theme.palette.text.disabled}`,
-  //   color: theme.palette.text.disabled,
-  //   width: 30,
-  //   height: 30,
-  //   borderRadius: "50%",
-  // },
   check: {
     fontSize: "1.2rem",
   },
-  // link: {
-  //   textDecoration: "none",
-  //   padding: ".5rem 2rem",
-  // },
-  // stepContainerActive: {
-  //   width: "100%",
-  //   border: "1px solid blue",
-  //   position: "relative",
-  //   "&::before": {
-  //     content: "''",
-  //     backgroundColor: theme.palette.primary.light,
-  //     position: "absolute",
-  //     top: "calc(50% - 1px)",
-  //     width: "100%",
-  //     height: "2px",
-  //     zIndex: -2,
-  //   },
-  // },
-  // stepContainerDisabled: {
-  //   border: "1px solid blue",
-  //   position: "relative",
-  //   "&::before": {
-  //     content: "''",
-  //     backgroundColor: theme.palette.text.disabled,
-  //     position: "absolute",
-  //     top: "calc(50% - 1px)",
-  //     width: "100%",
-  //     height: "2px",
-  //     zIndex: -2,
-  //   },
-  // },
-  // span: {
-  //   border: "1px solid red",
-  //   padding: "0 .5rem",
-  //   fontSize: ".9rem",
-  //   fontWeight: 300,
-  //   backgroundColor: "white",
-  //   [theme.breakpoints.down("sm")]: {
-  //     display: "none",
-  //   },
-  // },
 }))
 
 const CheckoutSteps = ({ step1, step2, step3 }) => {
   const classes = useStyles()
 
   return (
-    // <div className={classes.container}>
-    //   <ul className={classes.nav}>
-    //     {step1 ? (
-    //       <>
-    //         <li className={classes.liActive}>
-    //           <div className={classes.stepContainerActive}>
-    //             <div className={classes.circleActive}>
-    //               {step2 ? <CheckIcon className={classes.check} /> : "1"}
-    //             </div>
-    //           </div>
-    //           <span className={classes.span}>Shipping</span>
-    //         </li>
-    //       </>
-    //     ) : (
-    //       <div className={classes.stepContainerDisabled}>
-    //         <li className={classes.liDisabled}>
-    //           <div className={classes.circleDisabled}>1</div>
-    //           <span className={classes.span}>Shipping</span>
-    //         </li>
-    //       </div>
-    //     )}
-    //     {step2 ? (
-    //       <div className={classes.stepContainerActive}>
-    //         <li className={classes.liActive}>
-    //           <div className={classes.circleActive}>
-    //             {step3 ? <CheckIcon className={classes.check} /> : "2"}
-    //           </div>
-    //           <span className={classes.span}>Place order</span>
-    //         </li>
-    //       </div>
-    //     ) : (
-    //       <div className={classes.stepContainerDisabled}>
-    //         <li className={classes.liDisabled}>
-    //           <div className={classes.circleDisabled}>2</div>
-    //           <span className={classes.span}>Place order</span>
-    //         </li>
-    //       </div>
-    //     )}
-    //     {step3 ? (
-    //       <div>
-    //         <li className={classes.liActive}>
-    //           <div className={classes.circleActive}>3</div>
-    //           <span className={classes.span}>Pay</span>
-    //         </li>
-    //       </div>
-    //     ) : (
-    //       <div>
-    //         <li className={classes.liDisabled}>
-    //           <div className={classes.circleDisabled}>3</div>
-    //           <span className={classes.span}>Pay</span>
-    //         </li>
-    //       </div>
-    //     )}
-    //   </ul>
-    // </div>
     <div className={classes.root}>
       {step2 ? (
         step3 ? (
--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,203 +1,316 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { Grid, Button, TextField } from "@material-ui/core";
-import { withStyles, makeStyles } from "@material-ui/styles";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
-import Message from "../components/Message";
-import Loader from "../components/Loader";
-import CancelIcon from "@material-ui/icons/Cancel";
-import { getUserDetails, updateUserProfile } from "../actions/userActions";
-import { listMyOrders } from "../actions/orderActions";
-import { USER_UPDATE_PROFILE_RESET } from "../constants/userConstants";
+import React, { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux"
+import { Grid, Button, TextField } from "@material-ui/core"
+import { withStyles, makeStyles } from "@material-ui/styles"
+import IconButton from "@material-ui/core/IconButton"
+import Table from "@material-ui/core/Table"
+import TableBody from "@material-ui/core/TableBody"
+import TableCell from "@material-ui/core/TableCell"
+import TableContainer from "@material-ui/core/TableContainer"
+import TableHead from "@material-ui/core/TableHead"
+import TableRow from "@material-ui/core/TableRow"
+import Paper from "@material-ui/core/Paper"
+import Message from "../components/Message"
+import Loader from "../components/Loader"
+import DetailsIcon from "@material-ui/icons/MenuOpenRounded"
+import { getUserDetails, updateUserProfile } from "../actions/userActions"
+import { listMyOrders } from "../actions/orderActions"
+import { USER_UPDATE_PROFILE_RESET } from "../constants/userConstants"
+import EditIcon from "@material-ui/icons/Edit"
 
 const StyledTableCell = withStyles((theme) => ({
   root: {
-    ...theme.typography.source,
+    ...theme.typography.mont,
+    color: theme.palette.text.secondary,
+    fontSize: ".9rem",
+    border: "none",
+    outline: "none",
+    whiteSpace: "nowrap",
   },
-}))(TableCell);
+}))(TableCell)
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    "&:nth-of-type(even)": {
+      backgroundColor: "rgba(0,0,0,.02)",
+    },
+  },
+  body: {
+    position: "relative",
+    "&::after": {
+      content: "''",
+      width: "100%",
+      height: 1,
+      position: "absolute",
+      bottom: 0,
+      left: 0,
+      background: `linear-gradient(90deg, transparent, ${theme.palette.text.disabled}, transparent)`,
+      opacity: 0.5,
+    },
+  },
+}))(TableRow)
 
 const CssTextField = withStyles((theme) => ({
   root: {
+    ...theme.typography.mont,
     "& label.Mui-focused": {
-      color: theme.palette.primary.main,
+      color: theme.palette.secondary.light,
+    },
+    "& .MuiInput-focused fieldset": {
+      color: theme.palette.secondary.light,
     },
     "& .MuiOutlinedInput-root": {
-      ...theme.typography.source,
-      color: theme.palette.text.secondary,
-      "& fieldset": {
-        borderColor: theme.palette.text.secondary,
-      },
+      ...theme.typography.mont,
       "&:hover fieldset": {
-        borderColor: theme.palette.primary.light,
+        borderColor: theme.palette.secondary.light,
       },
       "&.Mui-focused fieldset": {
-        borderColor: theme.palette.primary.main,
+        borderColor: theme.palette.secondary.light,
       },
-      // "& .MuiOutlinedInput-input": {
-      //   padding: "1rem .8rem",
-      // },
     },
   },
-}))(TextField);
+}))(TextField)
 
 const useStyles = makeStyles((theme) => ({
   container: {
     ...theme.utils.container,
-    ...theme.typography.source,
-    marginTop: "15rem",
+    ...theme.typography.mont,
+    padding: "3rem",
+    fontWeight: 300,
   },
   title: {
-    ...theme.typography.prosto,
-    marginBottom: "2rem",
+    fontWeight: 300,
+    fontSize: "2.4rem",
+    color: theme.palette.text.primary,
+    margin: "3rem 0 5rem",
+  },
+  subtitle: {
+    fontWeight: 300,
+    fontSize: "1.8rem",
+    color: theme.palette.text.primary,
+    marginBottom: "3rem",
   },
   form: {
+    // border: "1px solid green",
     ...theme.flex.col,
     alignItems: "flex-start",
     "& > *": {
       marginBottom: "2rem",
-      width: "auto",
+      width: 300,
     },
   },
   submitBtn: {
-    backgroundColor: theme.palette.primary.main,
-    padding: ".5rem 3rem",
-    ...theme.typography.open,
-    fontWeight: 600,
+    ...theme.buttons.primary,
+    minWidth: 180,
+    marginTop: "1rem",
+    paddingTop: ".7rem",
+    paddingLeft: "2rem",
+    paddingRight: "3rem",
+    backgroundColor: theme.palette.secondary.light,
+    transition: "all .3s ease",
     "&:hover": {
-      backgroundColor: theme.palette.primary.dark,
+      backgroundColor: theme.palette.secondary.main,
+    },
+    [theme.breakpoints.down("md")]: {
+      fontSize: ".85rem",
+      flex: 1,
+      padding: ".3rem",
     },
   },
-  table: {
-    minWidth: 500,
+  editIcon: {
+    fontSize: "1.2rem",
+    marginRight: ".5rem",
+    marginBottom: ".2rem",
+    // [theme.breakpoints.down("md")]: {
+    //   fontSize: "1rem",
+    // },
+  },
+  myOrders: {
+    ...theme.flex.colStart,
+    paddingLeft: "2rem",
+    "& > *": {
+      marginLeft: "2rem",
+    },
+    position: "relative",
+    "&::after": {
+      content: "''",
+      width: 1,
+      height: "100%",
+      position: "absolute",
+      top: 0,
+      left: 0,
+      background: `linear-gradient(transparent, ${theme.palette.secondary.main}, transparent)`,
+    },
   },
   tableContainer: {
+    width: "100%",
     overflowX: "scroll",
   },
-}));
+  head: {
+    padding: ".5rem 1rem",
+    textTransform: "uppercase",
+    position: "relative",
+    "&::after": {
+      content: "''",
+      width: 1,
+      height: "100%",
+      position: "absolute",
+      top: 0,
+      left: 0,
+      background: `linear-gradient(transparent, ${theme.palette.secondary.light}, transparent)`,
+    },
+  },
+  info: {
+    fontSize: ".8rem",
+    textTransform: "uppercase",
+  },
+  success: {
+    fontSize: ".8rem",
+    color: theme.palette.common.success,
+  },
+  detailsBtn: {
+    marginLeft: ".5rem",
+  },
+  detailsIcon: {
+    color: theme.palette.text.disabled,
+  },
+}))
 
 const ProfileScreen = ({ location, history }) => {
-  const classes = useStyles();
+  const classes = useStyles()
 
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [passwordConfirm, setPasswordConfirm] = useState("");
-  const [message, setMessage] = useState(null);
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [passwordConfirm, setPasswordConfirm] = useState("")
+  const [message, setMessage] = useState(null)
 
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
   // STATE
-  const { loading, error, user } = useSelector((state) => state.userDetails);
-  const { loading: loadingOrders, error: errorOrders, orders } = useSelector(
-    (state) => state.orderMyList
-  );
-  const { userInfo } = useSelector((state) => state.userLogin);
-  const { success } = useSelector((state) => state.userUpdateProfile);
+  const { loading, error, user } = useSelector((state) => state.userDetails)
+  const {
+    loading: loadingOrders,
+    error: errorOrders,
+    orders,
+  } = useSelector((state) => state.orderMyList)
+  const { userInfo } = useSelector((state) => state.userLogin)
+  const { success } = useSelector((state) => state.userUpdateProfile)
 
   useEffect(() => {
     if (!userInfo) {
-      history.push("/login");
+      history.push("/login")
     } else {
-      dispatch(listMyOrders());
+      dispatch(listMyOrders())
       if (!user || !user.name || success) {
-        dispatch({ type: USER_UPDATE_PROFILE_RESET });
-        dispatch(getUserDetails("profile"));
+        dispatch({ type: USER_UPDATE_PROFILE_RESET })
+        dispatch(getUserDetails("profile"))
         // dispatch(listMyOrders());
       } else {
-        setName(user.name);
-        setEmail(user.email);
+        setName(user.name)
+        setEmail(user.email)
       }
     }
-  }, [userInfo, user, history, dispatch, success]);
+  }, [userInfo, user, history, dispatch, success])
 
   const submitHandler = (e) => {
-    e.preventDefault();
+    e.preventDefault()
     if (password !== passwordConfirm) {
-      setMessage("Passwords do not match");
+      setMessage("Passwords do not match")
     } else {
-      dispatch(updateUserProfile({ id: user._id, name, email, password }));
+      dispatch(updateUserProfile({ id: user._id, name, email, password }))
     }
-  };
+  }
 
   return (
-    <Grid container className={classes.container}>
-      <Grid item md={3}>
-        <h2 className={classes.title}>User profile</h2>
-        <form onSubmit={(e) => submitHandler(e)} className={classes.form}>
-          {loading && <Loader />}
-          {message && <Message variant="error" message={message} />}
-          {success && <Message variant="success" message="Profile updated" />}
-          {error && <Message variant="error" message={error} />}
-          <CssTextField
-            id="name"
-            label="Name"
-            variant="outlined"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <CssTextField
-            id="email"
-            label="Email"
-            variant="outlined"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <CssTextField
-            id="password"
-            type="password"
-            label="Password"
-            variant="outlined"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <CssTextField
-            id="confirmPassword"
-            type="password"
-            label="Confirm password"
-            variant="outlined"
-            value={passwordConfirm}
-            onChange={(e) => setPasswordConfirm(e.target.value)}
-          />
-          <Button type="submit" className={classes.submitBtn}>
-            Update
-          </Button>
-        </form>
-      </Grid>
-      <Grid item md={9}>
-        <h2 className={classes.title}>My orders</h2>
-        {loadingOrders ? (
-          <Loader />
-        ) : errorOrders ? (
-          <Message variant="error" message={error} />
-        ) : (
-          <div className={classes.tableContainer}>
-            <TableContainer component={Paper}>
-              <Table className={classes.table} aria-label="simple table">
+    <div className={classes.container}>
+      <h1 className={classes.title}>User profile</h1>
+      <Grid container>
+        <Grid item md={12} lg={4}>
+          <h2 className={classes.subtitle}>My data</h2>
+          <form onSubmit={(e) => submitHandler(e)} className={classes.form}>
+            {loading && <Loader />}
+            {message && <Message variant="error" message={message} />}
+            {success && <Message variant="success" message="Profile updated" />}
+            {error && <Message variant="error" message={error} />}
+            <CssTextField
+              id="name"
+              label="Name"
+              variant="outlined"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <CssTextField
+              id="email"
+              label="Email"
+              variant="outlined"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <CssTextField
+              id="password"
+              type="password"
+              label="Password"
+              variant="outlined"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <CssTextField
+              id="confirmPassword"
+              type="password"
+              label="Confirm password"
+              variant="outlined"
+              value={passwordConfirm}
+              onChange={(e) => setPasswordConfirm(e.target.value)}
+            />
+            <Button type="submit" className={classes.submitBtn}>
+              <EditIcon className={classes.editIcon} />
+              Update
+            </Button>
+          </form>
+        </Grid>
+        <Grid item xs={12} sm={12} md={12} lg={8} className={classes.myOrders}>
+          <h2 className={classes.subtitle}>My orders</h2>
+          {loadingOrders ? (
+            <Loader />
+          ) : errorOrders ? (
+            <Message variant="error" message={error} />
+          ) : (
+            <div className={classes.tableContainer}>
+              <Table className={classes.table}>
                 <TableHead>
                   <TableRow>
-                    <StyledTableCell className={classes.cell}>
-                      ID
+                    <StyledTableCell className={classes.head}>
+                      Id
                     </StyledTableCell>
-                    <StyledTableCell>DATE</StyledTableCell>
-                    <StyledTableCell>TOTAL</StyledTableCell>
-                    <StyledTableCell>PAID</StyledTableCell>
-                    <StyledTableCell>SENT</StyledTableCell>
-                    <StyledTableCell></StyledTableCell>
+                    <StyledTableCell className={classes.head}>
+                      Date
+                    </StyledTableCell>
+                    <StyledTableCell className={classes.head}>
+                      Total
+                    </StyledTableCell>
+                    <StyledTableCell className={classes.head}>
+                      Paid
+                    </StyledTableCell>
+                    <StyledTableCell className={classes.head}>
+                      Sent
+                    </StyledTableCell>
+                    <StyledTableCell className={classes.head}>
+                      Details
+                    </StyledTableCell>
                   </TableRow>
                 </TableHead>
                 <TableBody>
                   {orders.map((order) => (
-                    <TableRow key={order._id}>
+                    <StyledTableRow key={order._id}>
                       <StyledTableCell component="th" scope="row">
                         {order._id}
                       </StyledTableCell>
-                      <StyledTableCell component="th" scope="row">
+                      <StyledTableCell
+                        component="th"
+                        scope="row"
+                        style={{ fontSize: ".8rem" }}
+                      >
                         {order.createdAt.substring(0, 10)}
                       </StyledTableCell>
                       <StyledTableCell component="th" scope="row">
@@ -205,33 +318,39 @@ const ProfileScreen = ({ location, history }) => {
                       </StyledTableCell>
                       <StyledTableCell component="th" scope="row">
                         {order.isPaid ? (
-                          order.paidAt.substring(0, 10)
+                          <span className={classes.success}>
+                            {order.paidAt.substring(0, 10)}
+                          </span>
                         ) : (
-                          <CancelIcon />
+                          <span className={classes.info}>Not paid</span>
                         )}
                       </StyledTableCell>
                       <StyledTableCell component="th" scope="row">
                         {order.isDelivered ? (
-                          order.deliveredAt.substring(0, 10)
+                          <span className={classes.success}>
+                            {order.deliveredAt.substring(0, 10)}
+                          </span>
                         ) : (
-                          <CancelIcon />
+                          <span className={classes.info}>Not sent</span>
                         )}
                       </StyledTableCell>
                       <StyledTableCell component="th" scope="row">
                         <Link to={`/order/${order._id}`}>
-                          <button>details</button>
+                          <IconButton className={classes.detailsBtn}>
+                            <DetailsIcon className={classes.detailsIcon} />
+                          </IconButton>
                         </Link>
                       </StyledTableCell>
-                    </TableRow>
+                    </StyledTableRow>
                   ))}
                 </TableBody>
               </Table>
-            </TableContainer>
-          </div>
-        )}
+            </div>
+          )}
+        </Grid>
       </Grid>
-    </Grid>
-  );
-};
+    </div>
+  )
+}
 
-export default ProfileScreen;
+export default ProfileScreen
