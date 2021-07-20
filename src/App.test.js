236e1f0091446df9f2b775e7f5d049baeed1caee
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2021-07-20 23:39:41 +0200

    tests added

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,32 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react"
+import App from "./App"
+import { Provider } from "react-redux"
+import store from "./store"
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+it("renders navigation", () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  )
+  const linkElements = screen.getAllByText(/home/i)
+  expect(linkElements.length).toBe(2)
+})
+it("renders footer", () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  )
+  const linkElements = screen.getByText(/general conditions/i)
+  expect(linkElements).toBeInTheDocument()
+})
+// it("renders dynamic content", async () => {
+//   render(
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//   )
+//   const linkElements = await screen.findAllByText(/add to cart/i)
+//   expect(linkElements).toBeInstanceOf(Array)
+// })
